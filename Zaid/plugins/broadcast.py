308eb6cd813390d8e6c46b04646498a214346306
citysiders <140605897+citysiders@users.noreply.github.com>
2024-12-28 19:56:07 +0530

    Update broadcast.py

--- Zaid/plugins/broadcast.py
+++ Zaid/plugins/broadcast.py
@@ -6,8 +6,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = Client('broadcast_bot', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, sudoers=SUDOERS)
-
 api_id = '28045580'
 api_hash = '83001e24418ec7f54bfe95d4e390419f'
 bot_token = '7010331289:AAF68YreEc5JVgkoa9QvXld0fHDd7Ib9EtE'
