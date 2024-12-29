43428785ba39eabe82412fe67d352f891e60ac93
citysiders <140605897+citysiders@users.noreply.github.com>
2024-12-29 11:08:52 +0530

    Update start.py

--- Zaid/plugins/start.py
+++ Zaid/plugins/start.py
@@ -3,17 +3,19 @@ from Config import Config
 from telethon import events, Button
 
 PM_START_TEXT = """
-ʜᴇʏᴀ! {}
+Hello! {}
 
-• I am a simple music bot on Telegram, here to play your favorite tunes!.
-• Just let me know what song you want, and I’ll play it for you!.
-• This bot is just for listening to music; there are no additional management features
-• I can do a lot of things, but I’m still in development. Please handle me with care!.
+❂ I am a simple music bot on Telegram, here to play your favorite tunes!.
+🏎 Just let me know what song you want, and I’ll play it for you!.
+🏎 This bot is just for listening to music; there are no additional management features
+🏎 I can do a lot of things, but I’m still in development. Please handle me with care!.
 
   We are the Universe People – initially a network for selling VPS, we have since evolved into providing bots for user use.
 
-  Having queries - | Contact the Universe - Networks -☯- | @universe_we_are | -☯-
-➻ **ᴄʟɪᴄᴋ ᴏɴ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ**.
+ ✾ Having queries - | Contact the Universe - Networks -☯- \n| @universe_we_are | -☯-
+
+
+☣ **ᴄʟɪᴄᴋ ᴏɴ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ**.
 """
 
 @Zaid.on(events.NewMessage(pattern="^[?/]start$"))
@@ -29,7 +31,7 @@ async def start(event):
        return
 
     if event.is_group:
-       await event.reply("**ʜᴇʏ! ɪ'ᴍ ꜱᴛɪʟʟ ᴀʟɪᴠᴇ! and working fine ! Thank you for checking up!**")
+       await event.reply("🌠 **ʜᴇʏ! ɪ'ᴍ ꜱᴛɪʟʟ ᴀʟɪᴠᴇ! and working fine ! Thank you for checking up!**")
        return
 
 
@@ -40,5 +42,5 @@ async def _(event):
         return
     if event.is_private:
        await event.edit(PM_START_TEXT.format(event.sender.first_name), buttons=[
-        [Button.url("✨ ᴀᴅᴅ ᴍᴇ", f"https://t.me/{BOT_USERNAME}?startgroup=true"), Button.inline("🥀 ʜᴇʟᴘ", data="help")]])
+        [Button.url("➿ ᴀᴅᴅ ᴍᴇ", f"https://t.me/{BOT_USERNAME}?startgroup=true"), Button.inline("☏ ʜᴇʟᴘ", data="help")]])
        return
