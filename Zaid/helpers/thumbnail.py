8e7c2c39f82ab2eb537c398512a5ef80bd25284e
abitha000 <abithak000@yahoo.com>
2024-12-30 10:25:13 +0530

    Update thumbnail.py

--- Zaid/helpers/thumbnail.py
+++ Zaid/helpers/thumbnail.py
@@ -129,5 +129,7 @@ async def gen_thumb(videoid, chat_id):
         background.save(f"cache/{videoid}_kaithumb.png")
         return f"cache/{videoid}_kaithumb.png"
     except Exception as e:
-        print(e)
-        return f"cache/{videoid}_kaithumb.png"
+    print(e)
+    # Choose a random thumbnail from the list
+    random_thumb = random.choice(YOUTUBE_IMG_URL)
+    return random_thumb
